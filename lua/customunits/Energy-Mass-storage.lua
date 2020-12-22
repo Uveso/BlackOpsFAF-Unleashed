0a21a91b25144074eead568ad0c432989e7ca64a
Uveso <Archivar@Zwergenteam.de>
2020-12-22 06:23:26 +0100

    Update V19

--- lua/customunits/Energy-Mass-storage.lua
+++ lua/customunits/Energy-Mass-storage.lua
@@ -3,17 +3,18 @@
 -- Summary  :  BlackOps: Unleashed
 -- Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
 -----------------------------------------------------------------
+-- we need a new temlpate for T3EnergyStorage and T3MassStorage here
 UnitList = {
-    EnergyStorage = {
-        UEF = {'beb1106', 50}, -- Atlas (Mass & Energy Storage)
-        Cybran = {'brb1106', 50}, -- Stain (Mass & Energy Storage)
-        Aeon = {'bab1106', 50}, -- Vajun (Mass & Energy Storage)
-        Seraphim = {'bsb1106', 50}, -- Orba (Mass & Energy Storage)
-    },
-    MassStorage = {
-        UEF = {'beb1106', 50}, -- Atlas (Mass & Energy Storage)
-        Cybran = {'brb1106', 50}, -- Stain (Mass & Energy Storage)
-        Aeon = {'bab1106', 50}, -- Vajun (Mass & Energy Storage)
-        Seraphim = {'bsb1106', 50}, -- Orba (Mass & Energy Storage)
-    },
+--    EnergyStorage = {
+--        UEF = {'beb1106', 50}, -- Atlas (Mass & Energy Storage)
+--        Cybran = {'brb1106', 50}, -- Stain (Mass & Energy Storage)
+--        Aeon = {'bab1106', 50}, -- Vajun (Mass & Energy Storage)
+--        Seraphim = {'bsb1106', 50}, -- Orba (Mass & Energy Storage)
+--    },
+--    MassStorage = {
+--        UEF = {'beb1106', 50}, -- Atlas (Mass & Energy Storage)
+--        Cybran = {'brb1106', 50}, -- Stain (Mass & Energy Storage)
+--        Aeon = {'bab1106', 50}, -- Vajun (Mass & Energy Storage)
+--        Seraphim = {'bsb1106', 50}, -- Orba (Mass & Energy Storage)
+--    },
 }
